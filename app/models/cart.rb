d8158c5a35d8c20b2eea6b19b8424210fd071e81
github username <github email address>
2018-01-18 18:04:41 -0500

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
 
+  def total
+    self.items.map{|item| item.price}.reduce(:+)
+  end
+
+  def add_item(item)
+    Item.find(item).line_items << LineItem.new
+  end
 end
